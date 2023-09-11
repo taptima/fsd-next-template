a6cc817c83e7d26a976fc43efb99a198732d7517
Andrey Alekseev <andrey.a@prpl.global>
2023-09-11 18:43:22 +0300

    initial app configuration

---
+++ next.config.js
@@ -0,0 +1,100 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ['ru'],
+    defaultLocale: 'ru',
+  },
+  images: {
+    domains: [process.env.NEXT_PUBLIC_DOMAIN],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  headers() {
+    return [
+      {
+        source: '/fonts/(.*?)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
+  webpack(config) {
+    const originalEntry = config.entry;
+
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
+        entries['main.js'].unshift('./polyfills.js');
+      }
+
+      return entries;
+    };
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: 'svg-sprite-loader',
+    });
+
+    config.module.rules.push({
+      test: /\.graphql$/,
+      loader: 'graphql-tag/loader',
+      exclude: /node_modules/,
+    });
+
+    config.module.rules = config.module.rules.map((rule) => {
+      const { use } = rule;
+
+      if (!use || use.loader !== 'next-babel-loader') {
+        return rule;
+      }
+
+      const originalExcludeFunc = rule.exclude;
+      rule.exclude = (path) => {
+        if (/(swiper|dom7|slack)\/\.*/.test(path)) {
+          return false;
+        }
+
+        return originalExcludeFunc(path);
+      };
+
+      return rule;
+    });
+
+    return config;
+  },
+};
+
+/**
+ * @type {import('@sentry/nextjs').SentryWebpackPluginOptions}
+ **/
+const SentryWebpackPluginOptions = {
+  include: '.next',
+  ignore: ['node_modules'],
+  urlPrefix: '~/_next',
+  configFile: 'sentry.properties',
+};
+
+if (process.env.NODE_ENV === 'production' && Boolean(process.env.NEXT_PUBLIC_SENTRY_DSN)) {
+  module.exports = withSentryConfig(
+    {
+      ...nextConfig,
+      sentry: {
+        hideSourceMaps: true,
+      },
+    },
+    SentryWebpackPluginOptions,
+  );
+} else {
+  module.exports = nextConfig;
+}
