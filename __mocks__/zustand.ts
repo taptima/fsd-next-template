0284c413b4637de8ffb10f37083b9f0e3511f7b5
Andrey Alekseev <andrey.alekseev.taptima@gmail.com>
2023-10-13 17:01:50 +0300

    feat(FSK-64): add store testing example

---
+++ __mocks__/zustand.ts
@@ -0,0 +1,41 @@
+import { act } from '@testing-library/react';
+import * as zustand from 'zustand';
+
+const { create: actualCreate, createStore: actualCreateStore } =
+    jest.requireActual<typeof zustand>('zustand');
+
+export const storeResetFns = new Set<() => void>();
+
+export const create = (<T>() => {
+    console.log('zustand create mock');
+
+    return (stateCreator: zustand.StateCreator<T>) => {
+        const store = actualCreate(stateCreator);
+        const initialState = store.getState();
+        storeResetFns.add(() => {
+            store.setState(initialState, true);
+        });
+
+        return store;
+    };
+}) as typeof zustand.create;
+
+export const createStore = (<T>(stateCreator: zustand.StateCreator<T>) => {
+    console.log('zustand createStore mock');
+
+    const store = actualCreateStore(stateCreator);
+    const initialState = store.getState();
+    storeResetFns.add(() => {
+        store.setState(initialState, true);
+    });
+
+    return store;
+}) as typeof zustand.createStore;
+
+afterEach(() => {
+    act(() => {
+        storeResetFns.forEach((resetFn) => {
+            resetFn();
+        });
+    });
+});
