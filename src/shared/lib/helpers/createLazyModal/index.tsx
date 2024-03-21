fcd59919f233b856ec2bd44f0459a20347fef6e0
Dmitrii Zvezdetskii <zvezdetskiy@taptima.ru>
2024-03-21 17:00:29 +1000

    feat: add createLazyModal

---
+++ src/shared/lib/helpers/createLazyModal/index.tsx
@@ -0,0 +1,41 @@
+import { type ComponentType, useEffect, useState } from 'react';
+import dynamic, { type Loader } from 'next/dynamic';
+import { BlockingLoader } from 'shared/ui/feedback/BlockingLoader';
+
+// TODO: Заменить на пропсы из модалки
+type ModalProps = {
+    isOpen: boolean;
+};
+
+type BaseModalProps = Pick<ModalProps, 'isOpen'>;
+
+type Props<T extends BaseModalProps> = T & {
+    Modal: ComponentType<T>;
+};
+
+const LazyModalManager = <T extends BaseModalProps>(props: Props<T>) => {
+    const { Modal, isOpen, ...modalProps } = props;
+    const LazyModal = Modal as ComponentType<Omit<T, 'isOpen' | 'Modal'>>;
+    const [wasOpen, setWasOpen] = useState(false);
+
+    useEffect(() => {
+        setWasOpen((prev) => prev || isOpen);
+    }, [isOpen]);
+
+    if (!wasOpen) {
+        return null;
+    }
+
+    return <LazyModal isOpen={isOpen} {...modalProps} />;
+};
+
+export const createLazyModal = <T extends BaseModalProps>(loader: Loader<T>) => {
+    const DynamicModal = dynamic(loader, {
+        ssr: false,
+        loading: () => <BlockingLoader />,
+    });
+
+    return function LazyModal(props: T) {
+        return <LazyModalManager Modal={DynamicModal} {...props} />;
+    };
+};
---
+++ src/shared/ui/feedback/BlockingLoader/BlockingLoader.stories.tsx
@@ -0,0 +1,23 @@
+import type { Decorator, Meta, StoryFn, StoryObj } from '@storybook/react';
+import { BlockingLoader } from './index';
+
+const LayoutDecorator: Decorator = (Story: StoryFn) => {
+    return (
+        <div style={{ height: 500 }}>
+            <Story />
+        </div>
+    );
+};
+
+const meta: Meta<typeof BlockingLoader> = {
+    title: 'shared/feedback/BlockingLoader',
+    component: BlockingLoader,
+    tags: ['autodocs'],
+    decorators: [LayoutDecorator],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BlockingLoader>;
+
+export const Primary: Story = {};
