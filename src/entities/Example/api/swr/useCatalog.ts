5d087a237010d139086eac6a9b91591dfc3a72d1
Vladislav Dietrich <v.ditrih@taptima.ru>
2025-12-30 16:13:06 +1000

    fix(FSK-82): example catalog ssr

--- src/entities/Example/api/swr/useCatalog.ts
+++ src/entities/Example/api/swr/useCatalog.ts
@@ -2,18 +2,19 @@ import useSWR from 'swr';
 import type { CatalogFilter } from 'entities/Example/api/types/filter';
 import { composeSWRKey } from 'shared/lib/helpers/composeSWRKey';
 import { fetchCatalog } from 'entities/Example/api/request/fetchCatalog';
+import { useCatalogPageStore } from 'pages/example/CatalogPage/model/store/useCatalogPageStore';
 import { CATALOG_KEY } from './keys';
 
 export const useCatalog = () => {
-    // const category = useStore.use.category();
-    // const page = useStore.use.page();
-    // const pageSize = useStore.use.pageSize();
+    const category = useCatalogPageStore.use.category();
+    const page = useCatalogPageStore.use.page();
+    const pageSize = useCatalogPageStore.use.pageSize();
 
     return useSWR(
         composeSWRKey<CatalogFilter>(CATALOG_KEY, {
-            // category,
-            // page,
-            // pageSize,
+            category,
+            page,
+            pageSize,
         }),
         async ({ filters }) => {
             const data = await fetchCatalog(filters);
--- src/shared/types/api/filter.ts
+++ src/shared/types/api/filter.ts
@@ -1,4 +1,4 @@
 export type PaginationFilter = {
-    page?: number;
-    pageSize?: number;
+    page: number;
+    pageSize: number;
 };
