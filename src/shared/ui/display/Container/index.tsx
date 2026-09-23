452f29e0cf8dd5aa790e983110f8d8d16df265d2
Vladislav Dietrich <v.ditrih@taptima.ru>
2026-09-23 16:16:07 +1000

    chore(FSK-84): add container classname

--- src/shared/ui/display/Container/index.tsx
+++ src/shared/ui/display/Container/index.tsx
@@ -1,10 +1,12 @@
 import type { FC, PropsWithChildren } from 'react';
+import clsx from 'clsx';
+import type { Classname } from 'shared/types/styles';
 import styles from './styles.module.scss';
 
-type Props = PropsWithChildren;
+type Props = PropsWithChildren & Classname;
 
 export const Container: FC<Props> = (props) => {
-    const { children } = props;
+    const { children, className } = props;
 
-    return <div className={styles.wrapper}>{children}</div>;
+    return <div className={clsx(styles.wrapper, className)}>{children}</div>;
 };
